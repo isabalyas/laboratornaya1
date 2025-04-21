86844416792c2eeb49bf7768b47125176c6bce17
isabalyas <nebalyasnikov0@gmail.com>
2025-04-21 10:32:27 +0700

    diagonal1

--- rectangle.cpp
+++ rectangle.cpp
@@ -1,8 +1,31 @@
-Слиние ветки rectangle с веткой main
- Merge branch 'rectangle'
-# Пожалуйста, введите сообщение коммита, для объяснения, зачем нужно
-# это слияние, особенно, если это слияние обновленной вышестоящей
-# ветки в тематическую ветку.
-#
-# Строки, начинающиеся с «#» будут проигнорированы, а пустое сообщение
-# отменяет процесс коммита.
+#include <iostream>
+#include <cmath> 
+
+using namespace std;
+
+int main() {
+    cout << "Введите длину прямоугольника: ";
+    int length_rectangle;
+    cin >> length_rectangle;
+    
+    cout << "Введите ширину прямоугольника: ";
+    int width_rectangle;
+    cin >> width_rectangle;
+    
+    if (length_rectangle <= 0 || width_rectangle <= 0) 
+    {
+        cout << "Длина не может быть меньше или равна нулю" << endl;
+        return 0;
+    }
+    
+    int perimeter = (length_rectangle + width_rectangle) * 2;
+    int area = length_rectangle * width_rectangle;
+    float diagonal = sqrt(length_rectangle * length_rectangle + 
+                         width_rectangle * width_rectangle);
+    
+    cout << "Диагональ1: " << diagonal << endl;
+    cout << "Площадь: " << area << endl;
+    cout << "Периметр: " << perimeter << endl;
+    
+    return 0;
+}
